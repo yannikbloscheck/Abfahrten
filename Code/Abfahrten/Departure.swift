0a8611c6280ff291bc29ca381a294b532276a4e6
Yannik Bloscheck <yannikbloscheck@users.noreply.github.com>
2017-11-04 21:41:34 +0100

    Added markup to Departure.swift

--- Code/Abfahrten/Departure.swift
+++ Code/Abfahrten/Departure.swift
@@ -7,12 +7,26 @@ import Foundation
 /// - Since: 2017-10-15
 struct Departure: CustomStringConvertible, CustomDebugStringConvertible {
     // MARK: - Properties
+    
+    /// The line that will depart
     let line: String
+    
+    /// The type of transportation that will depart
     let type: Type
+    
+    /// The direction in which the line will depart
     let direction: String
+    
+    /// The time when the line will depart
     let time: String
+    
+    /// The delay with which the line will depart
     let delay: Int
+    
+    /// The platform where the line will depart from
     let platform: String
+    
+    /// A description of the departure
     var description: String {
         var description = "Departure of \(line) with a \(type.name().lowercased()) to \(direction) at \(time)"
         if delay > 1 {
@@ -23,6 +37,8 @@ struct Departure: CustomStringConvertible, CustomDebugStringConvertible {
         }
         return description
     }
+    
+    /// A description of the departure for debugging
     var debugDescription: String {
         return "Departure of \"\(line)\" with a \"\(type.name())\" to \"\(direction)\" at \"\(time)\" with a delay of \"\(delay)\" minutes from platform \"\(platform)\""
     }
@@ -30,6 +46,13 @@ struct Departure: CustomStringConvertible, CustomDebugStringConvertible {
     
     
     // MARK: - Initialization
+    
+    /// Initalize a departure
+    ///
+    /// - Parameter line: A line that will depart
+    /// - Parameter type: A type of transportation that will depart
+    /// - Parameter direction: A direction in which the line will depart
+    /// - Parameter time: A time when the line will depart
     init(of line: String, with type: Type, to direction: String, at time: String) {
         self.line = line
         self.type = type
@@ -40,6 +63,13 @@ struct Departure: CustomStringConvertible, CustomDebugStringConvertible {
     }
     
     
+    /// Initalize a departure
+    ///
+    /// - Parameter line: A line that will depart
+    /// - Parameter type: A type of transportation that will depart
+    /// - Parameter direction: A direction in which the line will depart
+    /// - Parameter time: A time when the line will depart
+    /// - Parameter platform: A platform where the line will depart from
     init(of line: String, with type: Type, to direction: String, at time: String, from platform: String) {
         self.line = line
         self.type = type
@@ -50,6 +80,13 @@ struct Departure: CustomStringConvertible, CustomDebugStringConvertible {
     }
     
     
+    /// Initalize a departure
+    ///
+    /// - Parameter line: A line that will depart
+    /// - Parameter type: A type of transportation that will depart
+    /// - Parameter direction: A direction in which the line will depart
+    /// - Parameter time: A time when the line will depart
+    /// - Parameter delay: A delay with which the line will depart
     init(of line: String, with type: Type, to direction: String, at time: String, with delay: Int) {
         self.line = line
         self.type = type
@@ -60,6 +97,14 @@ struct Departure: CustomStringConvertible, CustomDebugStringConvertible {
     }
     
     
+    /// Initalize a departure
+    ///
+    /// - Parameter line: A line that will depart
+    /// - Parameter type: A type of transportation that will depart
+    /// - Parameter direction: A direction in which the line will depart
+    /// - Parameter time: A time when the line will depart
+    /// - Parameter delay: A delay with which the line will depart
+    /// - Parameter platform: A platform where the line will depart from
     init(of line: String, with type: Type, to direction: String, at time: String, with delay: Int, from platform: String) {
         self.line = line
         self.type = type
